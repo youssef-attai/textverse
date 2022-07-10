60d701645d7c536cbd602e2149020a31078aaaad
Youssef Galal <youssefgalalnazem@gmail.com>
2022-07-10 22:14:34 +0200

    Create Story

--- creator/story.py
+++ creator/story.py
@@ -1,2 +1,10 @@
+from typing import Dict
+
+from creator.variable import Variable
+
+
 class Story:
-    pass
+    __variables: Dict[str, Variable]
+
+    def __init__(self, variables: dict) -> None:
+        self.__variables = variables
