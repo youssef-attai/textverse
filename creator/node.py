cf1ca7e5c0c5b24373b3be940594dc851835c7c6
Youssef Galal <youssefgalalnazem@gmail.com>
2022-07-10 22:06:05 +0200

    Initial Node structure

--- creator/node.py
+++ creator/node.py
@@ -1,2 +1,26 @@
+from typing import List
+
+from creator.option import Option
+
+
 class Node:
-    pass
+    __text: str
+    __options: list[Option]
+    __branches: list['Node']
+
+    def __init__(self, text: str, options: List[Option]) -> None:
+        self.__text = text
+        self.__options = options
+        self.__branches = list()
+
+    def set_text(self, text: str) -> None:
+        self.__text = text
+
+    def get_text(self) -> str:
+        return self.__text
+
+    def add_option(self, option: Option) -> None:
+        self.__options.append(option)
+
+    def add_branch(self, branch: 'Node') -> None:
+        self.__branches.append(branch)
